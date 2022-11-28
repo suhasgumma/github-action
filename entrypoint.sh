38f2159be920c4dac3c136444413df22c2615a11
Suhas Gumma <43647369+suhasgumma@users.noreply.github.com>
2022-11-28 11:11:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ ACCOUNT_OPT=$([ ! -z "$INPUT_ACCOUNT" ] && echo --account $INPUT_ACCOUNT --submi
 FAIL_THRESHOLD_OPT=$([ ! -z "$INPUT_FAILEDTHRESHOLD" ] && echo --fail-threshold $INPUT_FAILEDTHRESHOLD || echo "")
 SEVERITY_THRESHOLD_OPT=$([ ! -z "$INPUT_SEVERITYTHRESHOLD" ] && echo --severity-threshold $INPUT_SEVERITYTHRESHOLD || echo "")
 
-COMMAND="kubescape scan $FRAMEWORKS_CMD $CONTROLS_CMD $FILES --format json --format-version v2 --output output.json  --use-artifacts-from $ARTIFACTS_PATH && yes | kubescape fix output.json"
+COMMAND="kubescape scan $FRAMEWORKS_CMD $CONTROLS_CMD $FILES --format json --format-version v2 --output output.json  --use-artifacts-from $ARTIFACTS_PATH && yes | kubescape fix output.json && rm output.json"
 
 eval $COMMAND
 
