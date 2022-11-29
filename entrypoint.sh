e88ccc3c5a2d493069999fe707e572d04f4bb03e
suhasgumma <suhasgumma2001@gmail.com>
2022-11-29 07:55:51 +0530

    Testt

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,6 +40,7 @@ SEVERITY_THRESHOLD_OPT=$([ ! -z "$INPUT_SEVERITYTHRESHOLD" ] && echo --severity-
 
 if [ "$INPUT_SUGGESTFIX" = true];
 then
+    echo "I am here"
     COMMANDONE = "kubescape scan $FRAMEWORKS_CMD $CONTROLS_CMD $FILES --format json --format-version v2 --output output.json  --use-artifacts-from $ARTIFACTS_PATH"
     COMMANDTWO = "yes | kubescape fix output.json"
     COMMANDTHREE = "rm output.json"
@@ -49,6 +50,7 @@ then
     eval $COMMANDTHREE
 
 else
+    echo "No, I am here"
     COMMAND="kubescape scan $FRAMEWORKS_CMD $CONTROLS_CMD $FILES $ACCOUNT_OPT $FAIL_THRESHOLD_OPT $SEVERITY_THRESHOLD_OPT --format $INPUT_FORMAT --output $OUTPUT_FILE --use-artifacts-from $ARTIFACTS_PATH"
     eval $COMMAND
 
